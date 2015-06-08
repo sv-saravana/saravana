48e4b5355c772a9fedb5205e61f4a570ff403e7a
svasantha <svasantha@LINDCHN125.issnet.iss-shipping.com>
2015-06-08 11:11:54 +0530

    test

---
+++ src/com/example/tests/Test.java
@@ -0,0 +1,39 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://tamil.oneindia.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void test() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("டோலிவ�?ட�?டில�? தாக�?க�?ப�?பிடிக�?க எனக�?க�? சமத�?த�? போதாத�?: டாப�?ஸி")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
